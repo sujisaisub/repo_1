395354c84bceb6e758b13d43804ae35332d21565
sujirasa <sujitha.rasalingam91@gmail.com>
2020-09-03 15:03:50 -0400

    del

--- base/chatbot.py
+++ base/chatbot.py
@@ -1,6 +1,6 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-from chatterbot.trainers import ChatterBotCorpusTrainer
+#from chatterbot import ChatBot
+#from chatterbot.trainers import ListTrainer
+#from chatterbot.trainers import ChatterBotCorpusTrainer
 
 # Creating ChatBot Instance
 chatbot = None
